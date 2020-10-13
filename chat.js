5214448c935f2b5814dfa8e96f49cb6a8f928b93
Victor <victorabatistella@gmail.com>
2020-10-13 17:29:03 -0300

    chat reader python

--- chat.js
+++ chat.js
@@ -1,6 +1,11 @@
 const tmi = require('tmi.js');
+var express = require('express');
 
+/*********************************/
+// Twitch Communication
+/*********************************/
 
+// Constants for bot
 const opts = {
   identity: {
     username: 'maestro',
@@ -10,27 +15,48 @@ const opts = {
     'ourchestra_bot'
   ]
 };
-const client = new tmi.client(opts);
 
+// Connect on twtich
+const client = new tmi.client(opts);
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
-
 client.connect();
 
+// Data array
+var data = [];
+
+// Message Handler
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; }
 
   const commandName = msg.trim();
 
-  // If the command is known, let's execute it
-  if (commandName === '!oi') {
-    client.say(target, 'pau no cu de quem ta lendo.');
-    console.log('* Executed ${commandName} command');
-  } else {
-    console.log('* Unknown command ${commandName}');
+  // Commands
+  switch (commandName) {
+    case '!oi':
+      client.say(target, 'pau no cu de quem ta lendo.');
+      console.log('* Executed !oi command');
+      data.push("oi");
+      break;
+    case '!kill':
+      client.say(target, 'why tho?');
+      console.log('* Executed !kill command');
+      break;
   }
 }
 
 function onConnectedHandler (addr, port) {
-  console.log('* Connected to ${addr}:${port}');
+  console.log('* Connected');
 }
+
+/*********************************/
+// Python Communication
+/*********************************/
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(data)
+  data = []
+})
+app.listen(port);

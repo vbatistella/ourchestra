dad54a345b2fda1d288ca30596f8a5a0e88d4a44
Gabriel Massukado <gabriel.massukado@usp.br>
2020-10-15 15:57:29 -0300

    minor changes to test

--- soundtest.py
+++ soundtest.py
@@ -4,6 +4,8 @@ Clock.set_time(7)
 Scale.default = "chromatic"
 Root.default = 0
 
+#This is just a test file, assume it's not here
+
 def major_scale(note):
     major = [note, note+2, note+4, note+5, note+7, note+9, note+11, note+12]
     print(major)

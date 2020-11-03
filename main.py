9bbb8d74ee988db94848ecb2913acb0a3fd3e32b
Gabriel Massukado <gabriel.massukado@usp.br>
2020-11-03 17:56:01 -0300

    still doesnt work, but doesnt work better

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from FoxDot import *
+import os
+import math
 
 # I'm putting this here because I think it can hypothetically make a difference in a bizarre corner case
 Clock.set_time(7)
-Scale.default = "chromatic"
+Scale.default = "chromatic" #we want
 Root.default = 0
 
+DEBUG = True
 running = True
 
 # Kickers
@@ -20,18 +23,25 @@ def drum_kicker(queue):
     pass
 
 def main():
+    global running
     print("é hora, meus bacanos")
 
     melody_queue = []
     chord_queue = []
     drum_queue = []
+    tempo = var([1,2,3,4],4)
     
     # This starts FoxDot, the question is "when"
     Go()
+    at = 0
 
     # This is where we will be constantly checking for messages and actually do stuff
     while(running):
-        pass
+        _, beat = (int(Clock.now()), tempo)
+        print(beat)
+        if not beat == at:
+            print("bla")
+            at = beat
 
     print("'tis the end, fellas")
 

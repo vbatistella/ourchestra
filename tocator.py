5a53f3498851f577f5e2da8914a857bec1de99bc
Gabriel Massukado <gabriel.massukado@usp.br>
2020-11-10 17:26:10 -0300

    added some comments

--- tocator.py
+++ tocator.py
@@ -8,20 +8,22 @@ Root.default = 0
 DEBUG = False
 
 """
+THE TOCATOR
 The idea is to have a FoxDot player for each track and play them all simultaneously. 
+We named it like so due to the fact that it shouldn't be confused with FoxDot's players.
 
 m1 - the melody track, is on default on pluck
 c1 - the chord track, is on default on <something>
 x1 - the drum track, I really don't think it cares about the instrument, it just uses play
 """
 
-def single_note(notes = [], durations = []):
+def single_note(notes = [], durations=[]):
     if DEBUG:
         print("i am going to play the following notes")
         print(notes)
         print(durations)
 
-    m1 >> pluck(notes, dur =durations)
+    m1 >> pluck(notes, dur=durations)
 
 def chord(chords = [], durations = []):
     if DEBUG:
